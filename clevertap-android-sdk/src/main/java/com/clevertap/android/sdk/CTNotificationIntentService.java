807954c7a86a95ecc4dcaa885cc66d50f0e77629
liwei <wei.li@zenjoy.net>
2019-03-13 14:57:37 +0800

    Revert "[LW]Modify notification clicked events. (redirect start activity)"
    This reverts commit 91a1515792876a75dbb2c89b998ed4eac0d5fc4e.

--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTNotificationIntentService.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTNotificationIntentService.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import static com.clevertap.android.sdk.CTPushNotificationReceiver.DEEPLINK_ACTIVITY;
-
 public class CTNotificationIntentService extends IntentService {
 
     public final static String MAIN_ACTION = "com.clevertap.PUSH_EVENT";
@@ -28,7 +26,7 @@ public class CTNotificationIntentService extends IntentService {
             Logger.v("CTNotificationIntentService handling " + TYPE_BUTTON_CLICK);
             handleActionButtonClick(extras);
         } else {
-            Logger.v("CTNotificationIntentService: unhandled intent " + intent.getAction());
+            Logger.v("CTNotificationIntentService: unhandled intent "+intent.getAction());
         }
     }
 
@@ -41,31 +39,18 @@ public class CTNotificationIntentService extends IntentService {
             Context context = getApplicationContext();
             Intent launchIntent;
             if (dl != null) {
-                try {
-                    Class<?> webLinkActivity = Class.forName(DEEPLINK_ACTIVITY);
-                    if (webLinkActivity == null) {
-                        return;
-                    }
-                    launchIntent = new Intent(context, webLinkActivity);
-                    launchIntent.setData(Uri.parse(dl));
-                } catch (Exception e) {
-                    return;
-                }
-//                launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(dl));
-
+                launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(dl));
             } else {
                 launchIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
             }
 
             if (launchIntent == null) {
                 Logger.v("CTNotificationService: create launch intent.");
-                return;
+               return;
             }
-            CleverTapAPI.handleNotificationClicked(context, extras);
 
             launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
-            launchIntent.putExtra(CTPushNotificationReceiver.FROM_CLEVERTAP, CTPushNotificationReceiver.CLEVERTAP_NOTIFICATION_CLICKED);
             launchIntent.putExtras(extras);
             launchIntent.removeExtra("dl");
 
@@ -80,7 +65,7 @@ public class CTNotificationIntentService extends IntentService {
             sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)); // close the notification drawer
             startActivity(launchIntent);
         } catch (Throwable t) {
-            Logger.v("CTNotificationService: unable to process action button click:  " + t.getLocalizedMessage());
+            Logger.v("CTNotificationService: unable to process action button click:  "+ t.getLocalizedMessage());
         }
     }
 }
--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTPushNotificationReceiver.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTPushNotificationReceiver.java
@@ -6,51 +6,37 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import static com.clevertap.android.sdk.Constants.DEEP_LINK_KEY;
-
 
 public class CTPushNotificationReceiver extends BroadcastReceiver {
-    public static final String FROM_CLEVERTAP = "from_cleverTap";
-    public static final String CLEVERTAP_NOTIFICATION_CLICKED = "clever_tap_notification_clicked";
-    public final static String DEEPLINK_ACTIVITY = "com.mxtech.videoplayer.ad.online.mxexo.WebLinksRouterActivity";
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         try {
             Intent launchIntent;
+
             Bundle extras = intent.getExtras();
             if (extras == null) {
                 return;
             }
-            CleverTapAPI cleverTapAPI = CleverTapAPI.getDefaultInstance(context);
-            if (cleverTapAPI != null) {
-                cleverTapAPI.pushNotificationClickedEvent(extras);
-            }
 
-            if (extras.containsKey(DEEP_LINK_KEY)) {
-                try {
-                    Class<?> webLinkActivity = Class.forName(DEEPLINK_ACTIVITY);
-                    if (webLinkActivity == null) {
-                        return;
-                    }
-                    launchIntent = new Intent(context, webLinkActivity);
-                    launchIntent.setData(Uri.parse(intent.getStringExtra(DEEP_LINK_KEY)));
-                } catch (Exception e) {
-                    return;
-                }
+            if (extras.containsKey(Constants.DEEP_LINK_KEY)){
+                launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(intent.getStringExtra(Constants.DEEP_LINK_KEY)));
             } else {
                 launchIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
                 if (launchIntent == null) {
                     return;
                 }
             }
-            CleverTapAPI.handleNotificationClicked(context, extras);
+
+            CleverTapAPI.handleNotificationClicked(context,extras);
 
             launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
-            launchIntent.putExtra(FROM_CLEVERTAP, CLEVERTAP_NOTIFICATION_CLICKED);
             launchIntent.putExtras(extras);
+
+            //to prevent calling of pushNotificationClickedEvent(extras) in ActivityLifecycleCallback
+            launchIntent.putExtra(Constants.WZRK_FROM_KEY, Constants.WZRK_FROM);
 
             context.startActivity(launchIntent);
 
