17b9e18be332887188dce9cfa6e23d919eba4f3f
liwei <wei.li@zenjoy.net>
2022-09-27 17:28:05 +0800

    [LW] fix: correct build error.

--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTNotificationIntentService.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CTNotificationIntentService.java
@@ -78,7 +78,7 @@ public class CTNotificationIntentService extends IntentService {
                 }
 
             }
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
                 sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)); // close the notification drawer
             }
             startActivity(launchIntent);
